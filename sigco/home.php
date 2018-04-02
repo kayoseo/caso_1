c0721d3705b7161a80fefa4a163245bbd72fb34b
Luis Caceres <luimacaroster@gmail.com>
2018-04-02 12:39:27 -0300

    agrego el home

---
+++ sigco/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<meta charset="utf-8" />
+    <title>Home</title>
+    <link rel="stylesheet" href="./estilo.css" type="text/css" media="all" />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script> 
+
+
+<head>
+<h1>SIGCO</h1>
+</head>
+
+
+
+<body>
+	
+
+	   <nav>
+        <ul>
+            <li><a href="#">Inicio.</a></li>
+        </ul>
+    </nav>
+    <p>Sistema de Gestión de Comunidades.</p>
+</body>
+
+
+</html>
