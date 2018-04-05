7e2f93b99c55e5864c1c4cd1c30ca2c52e8258b2
Juan Román <juan_i_roman@hotmail.com>
2018-04-05 11:56:09 -0300

    form vecino visita

---
+++ sigco/nuevaVisita.php
@@ -0,0 +1,26 @@
+<form action="/my-handling-form-page" method="post">
+    <div>
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre"/>
+    </div>
+    <div>
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" id="apellidos" name="apellidos"/>
+    </div>
+    <div>
+        <label for="fecha">Fecha:</label>
+        <input type="date" id="fecha" name="fecha"/>
+    </div>
+    <div>
+        <label for="descripcion">Descripcion:</label>
+        <input type="text" id="descripcion" name="descripcion"/>
+    </div>
+    <div>
+        <label for="importe">Importe:</label>
+        <input type="importe" id="importe" />
+    </div>
+    <div>
+        <label for="administrador">Administrador:</label>
+        <input type="administrador" id="administrador" />
+    </div>
+</form>
--- sigco/nuevoVecino.php
+++ sigco/nuevoVecino.php
@@ -1,14 +1,30 @@
 <form action="/my-handling-form-page" method="post">
-    <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" />
+<div>
+        <label for="rut">Rut:</label>
+        <input type="text" id="rut" name="rut"/>
     </div>
     <div>
-        <label for="mail">E-mail:</label>
-        <input type="email" id="mail" />
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre"/>
     </div>
     <div>
-        <label for="msg">Message:</label>
-        <textarea id="msg"></textarea>
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" id="apellidos" name="apellidos"/>
+    </div>
+    <div>
+        <label for="direccion">Dirección:</label>
+        <input type="text" id="direccion" name="direccion"/>
+    </div>
+    <div>
+        <label for="codigo">Dirección:</label>
+        <input type="text" id="codigo" name="codigo"/>
+    </div>
+    <div>
+        <label for="ciudad">Ciudad:</label>
+        <input type="ciudad" id="ciudad" />
+    </div>
+    <div>
+        <label for="telefono">Telefono:</label>
+        <input type="telefono" id="telefono" />
     </div>
 </form>
